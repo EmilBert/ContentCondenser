9a739747848d70dcfa7767bae29aad33e75d70e6
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-01-03 16:27:48 +0100

    Modal working

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Modal from './Components/Modal'
 
 export default function App() {
+  
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  
   return (
     <View style={styles.container}>
-      <Modal>
-        <Text>Open up App.js to start working on your app!</Text>
+      
+      <Pressable onPress={() => setIsModalOpen(true)}>
+        <Text>korv</Text>
+      </Pressable>
+
+      <Modal handleClose={setIsModalOpen} isOpen={isModalOpen} >
+
+        <Text style={{fontSize: 32, fontWeight:"bold",}}>Wow what a modal!</Text>
+        
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan ex nec ultrices pulvinar. Quisque condimentum augue nisi, et sollicitudin tellus rhoncus a. In ac malesuada erat. In posuere sapien non tortor malesuada, ac imperdiet nibh euismod. Ut placerat urna a ligula maximus, a faucibus ex fringilla. Pellentesque venenatis quis leo in vulputate. Morbi fringilla urna nibh, nec venenatis nunc faucibus quis. Suspendisse eleifend, metus at ullamcorper hendrerit, ipsum tellus tempor neque, in rutrum leo ante vel sapien. Pellentesque efficitur velit sollicitudin magna facilisis, sed pretium magna varius. Donec nec elit sem. Nam pretium eget urna nec vestibulum. Curabitur in ex vehicula leo malesuada faucibus in ut lectus.</Text>
+        <Text style={{ fontWeight:"bold",}}> Author Linus Kaksson - mmm kakor   </Text>
+
         <StatusBar style="auto" />
       </Modal>
     </View>
--- Components/Modal.js
+++ Components/Modal.js
@@ -1,13 +1,94 @@
-import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import {Animated, Pressable, StyleSheet, Text, View } from 'react-native';
 
-const Modal = ({children}) => {
+const Modal = (props) => {
+    
+const fadeAnim = useRef(new Animated.Value(0)).current;
+const translateAnim = useRef(new Animated.Value(30)).current;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(translateAnim, {
+        useNativeDriver: true,
+        toValue: 0,
+        duration: 300,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      useNativeDriver: true,
+      toValue: 0,
+      duration: 0,
+    }).start();
+
+    Animated.timing(translateAnim, {
+        useNativeDriver: true,
+        toValue: 30,
+        duration: 0,
+    }).start();
+  };    
+
+    useEffect(() => {
+        if (props.isOpen) {
+            fadeIn();
+        } else {
+            fadeOut();
+        }
+    }, [props.isOpen]);
+
+    
     return (
-        <Pressable style={{backgroundColor: "green"}}>
-        <View>
-            {children}
-        </View>  
-       </Pressable>
+    <Animated.View style={ props.isOpen ? {...styles.background, ...styles.show, opacity: fadeAnim} : {...styles.background, opacity:fadeAnim, ...styles.hide, opacity: fadeAnim}}>
+        <Pressable onPress={() => props.handleClose(!props.isOpen)} style={styles.pressableArea}></Pressable>
+        <Animated.View style={{...styles.windowHolder, transform:[{translateY:translateAnim}]}}>
+            <View style={{...styles.window, ...props.style}}>
+                {props.children}
+            </View>
+        </Animated.View>
+    </Animated.View>  
     );
 }
 export default Modal;
+
+// Style
+const styles = StyleSheet.create({
+    pressableArea: {
+        width: "100%",
+        height: "100%",
+    },
+    background: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "color: rgba(0, 0, 0, 0.5)",
+        flexDirection:'column',
+        justifyContent: "center",
+        transition: "opacity 0.5s",
+    },
+    windowHolder: {
+        position: "absolute",
+        justifyContent: "center",
+        margin:30,
+    },
+    window: {
+        width: "100%",
+        padding: 30,
+        borderRadius: 10,
+        backgroundColor: "white",
+    },
+    show: {
+        opacity: 1,
+    },
+    hide: {
+        display: "none",
+        opacity: 0,
+    },
+});
