8e4b7cd02d8c7abdd2c51b69606e214dd8b3261c
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-01-03 07:29:12 +0100

    Modal start

---
+++ Components/Modal.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Modal = ({children}) => {
+    return (
+        <>
+        {children}
+        </>
+    );
+}
+
+  export default Modal;
