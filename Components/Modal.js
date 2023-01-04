87a0f9b23f2b9c98c889960b8a1dc2429f3dba33
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-01-04 14:21:07 +0100

    Code cleanup

--- Components/Modal.js
+++ Components/Modal.js
@@ -1,44 +1,42 @@
 import React, { useRef, useEffect, useState } from 'react';
-import {Animated, Pressable, StyleSheet, Text, View } from 'react-native';
+import {Animated, Pressable, StyleSheet, View } from 'react-native';
 
 const Modal = (props) => {
 
-//Modal animation state
-const [hideModal, setHideModal] = useState(!props.isOpen);
+    //Modal animation variables
+    const [hideModal, setHideModal] = useState(!props.isOpen);
+    const fadeAnim      = useRef(new Animated.Value(0)).current;
+    const translateAnim = useRef(new Animated.Value(30)).current;
 
-const fadeAnim = useRef(new Animated.Value(0)).current;
-const translateAnim = useRef(new Animated.Value(30)).current;
+    // Animation functions
+    const fadeIn = () => {
+        Animated.timing(fadeAnim, {
+                useNativeDriver: true,
+                toValue: 1,
+                duration: 300,
+        }).start();
 
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
+        Animated.timing(translateAnim, {
+                useNativeDriver: true,
+                toValue: 0,
+                duration: 300,
+        }).start();
+    };
+    const fadeOut = () => {
+        Animated.timing(fadeAnim, {
+            useNativeDriver: true,
+            toValue: 0,
+            duration: 300,
+        }).start();
 
-    Animated.timing(translateAnim, {
-        useNativeDriver: true,
-        toValue: 0,
-        duration: 300,
-    }).start();
-  };
+        Animated.timing(translateAnim, {
+            useNativeDriver: true,
+            toValue: 30,
+            duration: 300,
+        }).start();
+    };    
 
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 3 seconds
-    Animated.timing(fadeAnim, {
-      useNativeDriver: true,
-      toValue: 0,
-      duration: 300,
-    }).start();
-
-    Animated.timing(translateAnim, {
-        useNativeDriver: true,
-        toValue: 30,
-        duration: 300,
-    }).start();
-  };    
-
+    // Start animation when modal is opened or closed
     useEffect(() => {
         if (props.isOpen) {
             setHideModal(false);
@@ -46,16 +44,12 @@ const translateAnim = useRef(new Animated.Value(30)).current;
         } else {
             fadeOut();
             //Set interval to wait for animation to finish before hiding modal
-            setTimeout(() => {
-                setHideModal(true);
-            }
-            , 300);
+            setTimeout(() => setHideModal(true), 300);
         }
     }, [props.isOpen]);
 
-    
     return (
-    <Animated.View style={ !hideModal ? {...styles.background, ...styles.show, opacity: fadeAnim} : {...styles.background, opacity:fadeAnim, ...styles.hide, opacity: fadeAnim}}>
+    <Animated.View style={ hideModal ? {...styles.background, opacity:fadeAnim, ...styles.hide, opacity: fadeAnim} : {...styles.background, ...styles.show, opacity: fadeAnim}}>
         <Pressable onPress={() => props.handleClose(!props.isOpen)} style={styles.pressableArea}></Pressable>
         <Animated.View style={{...styles.windowHolder, transform:[{translateY:translateAnim}, {perspective: 1000}]}}>
             <View style={{...styles.window, ...props.style}}>
